6b39d5add8a99b631b91117a3b00831421b2b228
marinellanegrini <marinellanegrini1@gmail.com>
2019-05-18 19:08:16 +0200

    aggiunti alcuni metodi a PersistentManager

--- Foundation/PersistentManager.php
+++ Foundation/PersistentManager.php
@@ -17,8 +17,16 @@ class PersistentManager {
         return static::$instance;
     }
 
-    public function loadById ($obj, $id){
-        switch ($obj){
+
+    /**
+     * Metodo che effettua una load dato l'id e l'oggetto da recuperare
+     * @param $nameobj nome dell'oggetto
+     * @param $id dell'oggetto da recuperare
+     * @return ECategoria|ECibo|EIngrediente|ERicetta|EUtente|oggetto*
+     *
+     */
+    public function loadById ($nameobj, $id){
+        switch ($nameobj){
             case "categoria":
                 $fcat = new FCategoria();
                 $ret = $fcat->loadById($id);
@@ -56,7 +64,106 @@ class PersistentManager {
         }
         return $ret;
 
-}
+    }
+
+
+    /**
+     * Metodo che effettua una store di un oggetto
+     * @param $obj oggetto da salvare
+     * @return bool|id dell'oggetto recuperato
+     */
+    public function store($obj){
+        $nameobj = get_class($obj);
+        switch ($nameobj){
+            case "ECategoria":
+                $fcat = new FCategoria();
+                $id = $fcat->store($obj);
+                break;
+            case "ECibo":
+                $fcib = new FCibo();
+                $id = $fcib->store($obj);
+                break;
+            case "ECommento":
+                $fcom = new FCommento();
+                $id = $fcom->store($obj);
+                break;
+            case "EIngrediente":
+                $fi = new FIngrediente();
+                $id = $fi->store($obj);
+                break;
+            case "ERicetta":
+                $fr = new FRicetta();
+                $id = $fr->store($obj);
+                break;
+            case "EUtente":
+                $fu = new FUtente();
+                $id = $fu->store($obj);
+                break;
+            default:
+                $id = false;
+        }
+        return $id;
+
+    }
+
+
+    /**
+     * @param $idric idricetta
+     * @param $idingr iduingrediente
+     * @return bool esito
+     */
+    public function storeIngrRicetta($idric, $idingr){
+        $frti = new FRictoIngr();
+        $ret = $frti->store($idric,$idingr);
+        return $ret;
+    }
+
+    /**
+     * @param $nameobj nome dell'oggetto
+     * @param $id id dell'oggetto
+     * @param $attr attributo da aggiornare
+     * @param $val valore da impostare
+     * @return bool esito aggiornamento
+     */
+    public function update($nameobj, $id, $attr, $val){
+        switch ($nameobj){
+            case "categoria":
+                $fcat = new FCategoria();
+                $ret = $fcat->update($id,$attr,$val);
+                break;
+            case "cibo":
+                $fcib = new FCibo();
+                $ret = $fcib->update($id,$attr,$val);
+                break;
+            case "commento":
+                $fcom = new FCommento();
+                $ret = $fcom->update($id,$attr,$val);
+                break;
+            case "ingrediente":
+                $fi = new FIngrediente();
+                $ret = $fi->update($id,$attr,$val);
+                break;
+            case "ricetta":
+                $fr = new FRicetta();
+                $ret = $fr->update($id,$attr,$val);
+                break;
+            case "rictoingr":
+                $frti = new FRictoIngr();
+                $ret = $frti->update($id,$attr,$val);
+                break;
+            case "utente":
+                $fu = new FUtente();
+                $ret = $fu->update($id,$attr,$val);
+                break;
+            case "utprefric":
+                $fupr = new FUtPrefRic();
+                $ret = $fupr->update($id,$attr,$val);
+                break;
+            default:
+                $ret = null;
+        }
+        return $ret;
+    }
 
 
 
