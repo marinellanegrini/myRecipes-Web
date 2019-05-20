2d571942ba1dc89ab78eb42ae86a8abf79a08642
valeriaparascandolo <valeriaparascandolo@libero.it>
2019-05-20 12:44:50 +0200

    metodo selezionaRicetta

--- Controller/GestioneRicette.php
+++ Controller/GestioneRicette.php
@@ -2,6 +2,16 @@
 
 class GestioneRicette {
 
+    /**
+     * Metodo che dato l'id restituisce una ricetta
+     * @param $id identificativo ricetta
+     * @return oggetto ERicetta
+     */
+    public function selezionaRicetta($id){
+        $pm = PersistentManager::getInstance();
+        $ret = $pm->loadById("ricetta",$id);
+        return $ret;
+    }
 	
 }
 
