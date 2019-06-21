1f4c8429a7ac00b79c7e30add4e8698ae6880984
Arianna Tusi <ariannatusi@MacBook-Pro-di-Arianna.local>
2019-06-21 10:57:31 +0200

    Foto

--- config.inc.php
+++ config.inc.php
@@ -1,7 +1,1 @@
-<<<<<<< HEAD
-<?php $GLOBALS['database']= 'id10010622_ciao'; $GLOBALS['username']=  'root'; $GLOBALS['password']= 'pippo';?>
-=======
-
-<?php $GLOBALS['database']= 'myRecipes'; $GLOBALS['username']=  'root'; $GLOBALS['password']= 'pippo';?>
-
->>>>>>> 5ca41a53db76007cf81275eac1aa8a8d0af615d1
+<?php $GLOBALS['database']= 'ciao'; $GLOBALS['username']=  'root'; $GLOBALS['password']= 'pippo';?>
